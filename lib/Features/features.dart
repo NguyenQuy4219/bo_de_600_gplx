dd9a63a8c97c04ccd4a7a151d3a9fbbdc0b4cc03
quyB2105718 <nguyenquy4219@gmail.com>
2025-05-05 11:09:24 +0700

    Update Main page

---
+++ lib/Features/features.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String, dynamic>> features = [
+  {'title': 'Đề ngẫu nhiên', 'color': Colors.orange, 'icon': Icons.shuffle},
+  {
+    'title': 'Thi theo bộ đề',
+    'color': Colors.redAccent,
+    'icon': Icons.library_books
+  },
+  {'title': 'Xem câu bị sai', 'color': Colors.lightGreen, 'icon': Icons.error},
+  {'title': 'Ôn tập câu hỏi', 'color': Colors.teal, 'icon': Icons.school},
+  {'title': 'Các biển báo', 'color': Colors.blue, 'icon': Icons.traffic},
+  {
+    'title': 'Mẹo ghi nhớ',
+    'color': Colors.purple,
+    'icon': Icons.tips_and_updates
+  },
+  {'title': '20 câu điểm liệt', 'color': Colors.brown, 'icon': Icons.warning},
+  {
+    'title': 'Top 50 câu sai',
+    'color': Colors.blueGrey,
+    'icon': Icons.leaderboard
+  },
+  {
+    'title': '120 mô phỏng',
+    'color': Colors.lightBlueAccent,
+    'icon': Icons.play_circle
+  },
+  {'title': 'Bỏ quảng cáo', 'color': Colors.red, 'icon': Icons.block},
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '/Features/features.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,45 +32,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Map<String, dynamic>> features = [
-    {'title': 'Đề ngẫu nhiên', 'color': Colors.orange, 'icon': Icons.shuffle},
-    {
-      'title': 'Thi theo bộ đề',
-      'color': Colors.redAccent,
-      'icon': Icons.library_books
-    },
-    {
-      'title': 'Xem câu bị sai',
-      'color': Colors.lightGreen,
-      'icon': Icons.error
-    },
-    {'title': 'Ôn tập câu hỏi', 'color': Colors.teal, 'icon': Icons.school},
-    {'title': 'Các biển báo', 'color': Colors.blue, 'icon': Icons.traffic},
-    {
-      'title': 'Mẹo ghi nhớ',
-      'color': Colors.purple,
-      'icon': Icons.tips_and_updates
-    },
-    {'title': '20 câu điểm liệt', 'color': Colors.brown, 'icon': Icons.warning},
-    {
-      'title': 'Top 50 câu sai',
-      'color': Colors.blueGrey,
-      'icon': Icons.leaderboard
-    },
-    {
-      'title': '120 mô phỏng',
-      'color': Colors.lightBlueAccent,
-      'icon': Icons.play_circle
-    },
-    {'title': 'Bỏ quảng cáo', 'color': Colors.red, 'icon': Icons.block},
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                    content: Text('Chức năng cài đặt chưa được hỗ trợ')),
+              );
+            },
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
